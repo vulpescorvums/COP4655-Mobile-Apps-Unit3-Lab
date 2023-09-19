86fd9c5e4ac02341baad6324bbd1ccbe61116ed5
Ky <kmallonn2021@fau.edu>
2023-09-18 20:52:25 -0400

    Completed parts 7-10
    Added a collection view that pulls album artwork from an API in a new "AlbumCell" file. Also additional layout changes.

---
+++ lab-tunley/lab-tunley/AlbumCell.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumCell.swift
+//  lab-tunley
+//
+//  Created by Ky on 9/18/23.
+//
+
+import UIKit
+
+class AlbumCell: UICollectionViewCell {
+    
+    @IBOutlet weak var albumImageView: UIImageView!
+}
--- lab-tunley/lab-tunley/AlbumsViewController.swift
+++ lab-tunley/lab-tunley/AlbumsViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import Nuke
 
-class AlbumsViewController: UIViewController {
+class AlbumsViewController: UIViewController, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // the number of items shown should be the number of albums we have.
+        albums.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // Get a collection view cell (based in the identifier you set in storyboard) and cast it to our custom AlbumCell
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AlbumCell", for: indexPath) as! AlbumCell
+
+            // Use the indexPath.item to index into the albums array to get the corresponding album
+            let album = albums[indexPath.item]
+
+            // Get the artwork image url
+            let imageUrl = album.artworkUrl100
+
+            // Set the image on the image view of the cell
+            Nuke.loadImage(with: imageUrl, into: cell.albumImageView)
+
+            return cell
+    }
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     
     var albums: [Album] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        collectionView.dataSource = self
         // Create a search URL for fetching albums (`entity=album`)
         let url = URL(string: "https://itunes.apple.com/search?term=sum_41&attribute=artistTerm&entity=album&media=music")!
         let request = URLRequest(url: url)
@@ -37,7 +62,10 @@ class AlbumsViewController: UIViewController {
                 // Try to parse the response into our custom model
                 let response = try decoder.decode(AlbumSearchResponse.self, from: data)
                 let albums = response.results
-                self?.albums = albums
+                DispatchQueue.main.async {
+                    self?.albums = albums
+                    self?.collectionView.reloadData()
+                }
                 print(albums)
             } catch {
                 print(error.localizedDescription)
@@ -47,6 +75,27 @@ class AlbumsViewController: UIViewController {
         // Initiate the network request
         task.resume()
 
+        // Get a reference to the collection view's layout
+        // We want to dynamically size the cells for the available space and desired number of columns.
+        // NOTE: This collection view scrolls vertically, but collection views can alternatively scroll horizontally.
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+
+        // The minimum spacing between adjacent cells (left / right, in vertical scrolling collection)
+        // Set this to taste.
+        layout.minimumInteritemSpacing = 4
+
+        // The minimum spacing between adjacent cells (top / bottom, in vertical scrolling collection)
+        // Set this to taste.
+        layout.minimumLineSpacing = 4
+
+        // Set this to however many columns you want to show in the collection.
+        let numberOfColumns: CGFloat = 3
+
+        // Calculate the width each cell need to be to fit the number of columns, taking into account the spacing between cells.
+        let width = (collectionView.bounds.width - layout.minimumInteritemSpacing * (numberOfColumns - 1)) / numberOfColumns
+
+        // Set the size that each tem/cell should display at
+        layout.itemSize = CGSize(width: width, height: width)
 
     }
     
